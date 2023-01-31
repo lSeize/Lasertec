2546cd342a0715cc7dd26e2c81322e4bed7e1228
Seize <ogeidgluz@gmail.com>
2023-01-31 16:33:48 -0300

    corrigindo erro crsf 3 add midleware

--- Lasertec/settings.py
+++ Lasertec/settings.py
@@ -48,6 +48,7 @@ MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.csrf.middleware.CsrfMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
